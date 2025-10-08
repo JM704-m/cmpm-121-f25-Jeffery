6533e9408e71ff58b920b13b35b6aa03a0a97ead
JM704-m <myx704ri@gmail.com>
2025-10-08 05:24:07 +0000

    Added an emoticon button

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,10 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const butn: HTMLButtonElement = document.createElement("button");
+  butn.type = "button";
+  butn.innerHTML = "😟";
+  document.body.append(butn);
+});
